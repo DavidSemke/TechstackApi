3694b0efddb5f127408a57b101dfb978dfa3d73a
David Semke <david@semke.ca>
2025-05-16 22:30:08 -0700

    Dbmodel(profiles): Add 'url' field to ProfileSerializer

--- src/apps/profiles/serializers.py
+++ src/apps/profiles/serializers.py
@@ -8,4 +8,4 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ["owner", "pic", "bio", "followers"]
+        fields = ["url", "owner", "pic", "bio", "followers"]
