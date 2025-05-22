ecf8591dddcd4e0420d7d4fb610de05cc2fda9cd
David Semke <david@semke.ca>
2025-05-21 22:40:29 -0700

    Fix(posts): Add str representation for Tag model

--- src/apps/posts/models.py
+++ src/apps/posts/models.py
@@ -25,6 +25,9 @@ class Tag(models.Model):
         ],
     )
 
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100, validators=[MinLengthValidator(20)])
