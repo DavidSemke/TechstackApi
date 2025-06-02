6ce18c6c33a149c9967f55d020e7b9e0fee41f01
David Semke <david@semke.ca>
2025-06-01 19:02:42 -0700

    Feat(posts): Add like_count and dislike_count fields to Post and Comment serializers

--- src/apps/posts/serializers.py
+++ src/apps/posts/serializers.py
@@ -13,6 +13,8 @@ class PostSerializer(serials.HyperlinkedModelSerializer):
     owner = serials.ReadOnlyField(source="owner.username")
     publish_date = serials.ReadOnlyField()
     tags = serials.StringRelatedField(many=True)
+    like_count = serials.SerializerMethodField()
+    dislike_count = serials.SerializerMethodField()
 
     class Meta:
         model = app_models.Post
@@ -24,16 +26,49 @@ class PostSerializer(serials.HyperlinkedModelSerializer):
             "publish_date",
             "last_modified_date",
             "content",
+            "like_count",
+            "dislike_count",
             "tags",
         ]
+
+    def get_like_count(self, obj):
+        return app_models.Reaction.objects.filter(
+            type=app_models.Reaction.ReactionType.LIKE, post=obj
+        ).count()
+
+    def get_dislike_count(self, obj):
+        return app_models.Reaction.objects.filter(
+            type=app_models.Reaction.ReactionType.DISLIKE, post=obj
+        ).count()
 
 
 class CommentSerializer(serials.HyperlinkedModelSerializer):
     owner = serials.ReadOnlyField(source="owner.username")
+    like_count = serials.SerializerMethodField()
+    dislike_count = serials.SerializerMethodField()
 
     class Meta:
         model = app_models.Comment
-        fields = ["url", "owner", "post", "create_date", "content", "reply_to"]
+        fields = [
+            "url",
+            "owner",
+            "post",
+            "create_date",
+            "content",
+            "reply_to",
+            "like_count",
+            "dislike_count",
+        ]
+
+    def get_like_count(self, obj):
+        return app_models.Reaction.objects.filter(
+            type=app_models.Reaction.ReactionType.LIKE, comment=obj
+        ).count()
+
+    def get_dislike_count(self, obj):
+        return app_models.Reaction.objects.filter(
+            type=app_models.Reaction.ReactionType.DISLIKE, comment=obj
+        ).count()
 
 
 class ReactionSerializer(serials.HyperlinkedModelSerializer):
