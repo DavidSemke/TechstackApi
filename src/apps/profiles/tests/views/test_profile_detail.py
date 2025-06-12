e542d102add7c68cdc02cce7612cca70c2191952
David Semke <david@semke.ca>
2025-06-11 19:27:04 -0700

    Test(profiles): Tests for profile detail view

---
+++ src/apps/profiles/tests/views/test_profile_detail.py
@@ -0,0 +1,102 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from ....core.model_factories import UserFactory
+
+
+class ProfileDetailTest(APITestCase):
+    def setUp(self):
+        self.user1 = UserFactory()
+        self.url = reverse("profile-detail", kwargs={"pk": self.user1.profile.id})
+        return super().setUp()
+
+    def test_get_guest(self):
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_post_put_patch_delete_guest(self):
+        methods = ["post", "put", "patch", "delete"]
+
+        for method in methods:
+            method_func = getattr(self.client, method)
+
+            with self.assertLogs("django.request", level="WARNING"):
+                res = method_func(self.url)
+
+            self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post_put_patch_delete_login(self):
+        user2 = UserFactory()
+
+        tokens = self.client.post(
+            reverse("jwt-create"), {"username": user2.username, "password": "password"}
+        ).json()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {tokens['access']}")
+
+        methods = ["post", "put", "patch", "delete"]
+
+        for method in methods:
+            method_func = getattr(self.client, method)
+
+            with self.assertLogs("django.request", level="WARNING"):
+                res = method_func(self.url)
+
+            self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_put_login_profile_owner(self):
+        tokens = self.client.post(
+            reverse("jwt-create"),
+            {"username": self.user1.username, "password": "password"},
+        ).json()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {tokens['access']}")
+
+        profile1 = self.user1.profile
+        new_bio = "put"
+        self.assertNotEqual(profile1.bio, new_bio)
+
+        res = self.client.put(
+            self.url,
+            {"bio": new_bio},
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        profile1.refresh_from_db()
+        self.assertEqual(profile1.bio, new_bio)
+
+    def test_patch_login_profile_owner(self):
+        tokens = self.client.post(
+            reverse("jwt-create"),
+            {"username": self.user1.username, "password": "password"},
+        ).json()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {tokens['access']}")
+
+        profile1 = self.user1.profile
+        new_bio = "patch"
+        self.assertNotEqual(profile1.bio, new_bio)
+
+        res = self.client.patch(
+            self.url,
+            {"bio": new_bio},
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        profile1.refresh_from_db()
+        self.assertEqual(profile1.bio, new_bio)
+
+    def test_post_delete_login_profile_owner(self):
+        tokens = self.client.post(
+            reverse("jwt-create"),
+            {"username": self.user1.username, "password": "password"},
+        ).json()
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {tokens['access']}")
+
+        methods = ["post", "delete"]
+
+        for method in methods:
+            method_func = getattr(self.client, method)
+
+            with self.assertLogs("django.request", level="WARNING"):
+                res = method_func(self.url)
+
+            self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
