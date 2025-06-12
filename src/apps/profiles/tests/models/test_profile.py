0a500880a17e7c0426f86e8711356edd2b9feb28
David Semke <david@semke.ca>
2025-06-11 19:25:34 -0700

    Test(profiles): Tests for Profile model

---
+++ src/apps/profiles/tests/models/test_profile.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from ...models import Profile
+
+
+class ProfileModelTest(TestCase):
+    def test_profile_created_with_user(self):
+        User = get_user_model()
+
+        # UserFactory disables the signal used to create a profile
+        # Use the User model here instead
+        user = User.objects.create(username="username", password="password")
+
+        try:
+            Profile.objects.get(owner=user)
+        except Profile.DoesNotExist:
+            self.fail("Profile was not created with user")
